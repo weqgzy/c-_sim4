276b1ff30147cf63c223b3f16c3e1b21cfb43b4e
SAVAGE <proj_1>
2023-04-17 12:28:36 +0300

    all_done3

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int lenArray = ReadInt("введите длинну массива");
+
+System.Console.Write("[ ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1 , 99);
+    System.Console.Write(randomArray[i] + ", ");
+}
+
+int ReadInt(string massage)
+{
+    System.Console.Write(massage + ":");
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+
+System.Console.Write("]");
